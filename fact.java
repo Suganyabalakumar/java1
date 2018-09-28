a7082e3f72d00c1b69cb271496010403944bbb06
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-28 13:59:38 +0000

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class guvi3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int f=1;
+        for(int i=1;i<=n;i++)
+        {
+        	f=f*i;
+
+        }
+        System.out.println(f);
+	}
+
+}
