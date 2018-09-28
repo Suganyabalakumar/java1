c92b872cb18255fde67e1680b98d476f4897a7f3
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-28 12:39:17 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class odd{
+
+     public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         int n1=sc.nextInt();
+        int p=0;
+        for(int i=n+1;i<n1;i++)
+          if(i%2==0)
+          {
+          
+        System.out.print(i+" ");
+          }
+         
+     }
+}
