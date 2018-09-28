e218753f9ef57389fb0f0021fc9fbad4c2582b2c
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-28 12:19:43 +0530

    Update pow.java

--- pow.java
+++ pow.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class pow{
+public class array{
 
      public static void main(String []args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
            int n1=sc.nextInt();
-        System.out.println(Math.pow(n,n1));
+        System.out.println(Math.round(Math.pow(n,n1)));
      }
 }
