9e6a6d90afbf6d861d95895e673dfeadf2fa3f9b
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-28 19:42:17 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class guvi3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=5;i++)
+        {
+        
+			int o=n*i;
+         System.out.println(o);
+	}
+	}
+
+}
