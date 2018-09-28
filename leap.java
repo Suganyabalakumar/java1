842ac48f30717a033d7924519006107491cd2675
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-28 11:56:17 +0530

    Update leap.java

--- leap.java
+++ leap.java
@@ -4,7 +4,7 @@ public class leap{
      public static void main(String []args){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        if((a%4==0 && a%400==0) ||a%100!=0)
+        if((a%4==0 || a%400==0)  && a%100!=0)
         {
             System.out.println("yes");
         }
