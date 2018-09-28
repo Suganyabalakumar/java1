5b8e0596c539f354fa225bcefdf4b5b2d11efdc0
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-28 11:31:20 +0530

    Create let.java

---
+++ let.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class let{
+
+     public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        char n=sc.next().charAt(0);
+        if(Character.isLetter(n))
+        {
+            System.out.println("Alphabet");
+        }
+    
+     else
+     {
+            System.out.println("No");
+      
+     }
+     }
+     
+}
