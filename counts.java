7de486e3f1f09f1f6271e2331f713f6f75408f11
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-29 10:31:04 +0530

    Create counts.java

---
+++ counts.java
@@ -0,0 +1,17 @@
+package dd;
+
+import java.util.Scanner;
+
+public class oo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int c=0;
+		String[] sd=s.split(" ");
+		int t=sd.length;
+		System.out.println(t);
+	}
+}
+	
