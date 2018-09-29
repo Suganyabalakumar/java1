a6ce542d339261cda220fc65082594a0e5a826ea
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-29 10:45:24 +0530

    Create stringword.java

---
+++ stringword.java
@@ -0,0 +1,22 @@
+package dd;
+
+import java.util.Scanner;
+
+public class oo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int c=0;
+		String[] sd=s.split(" ");
+		for(int i=0;i<sd.length;i++)
+		
+		{
+			c++;
+			}
+		System.out.println(c);
+		}
+		
+}
+	
