6d0c574dab083858fd6bb710487b97866590472a
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-30 18:42:25 +0530

    Rename count1.java to edit.java

---
+++ edit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class count1 {
+	public static void main(String s[])
+	{ 
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		System.out.println(s1);
+		}
+	}
+
+}
