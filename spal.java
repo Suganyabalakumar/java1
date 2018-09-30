80fdb9f87c5e60b9e6ba14b144754858d5c4020e
Suganyabalakumar <36624051+Suganyabalakumar@users.noreply.github.com>
2018-09-30 17:28:51 +0530

    Create spal.java

---
+++ spal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class gg {
+
+	public static void main(String ai[]) 
+	{
+	Scanner sc=new Scanner(System.in);
+	String s=sc.nextLine();
+	String sd="";
+		for(int i=s.length()-1;i>=0;i--)
+		 {
+		sd=sd+s.charAt(i);
+	     } 
+		if(sd.equals(s))
+		{
+		System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}}
